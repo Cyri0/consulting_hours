804ab61b490bae83795a317ee1fe9e6adf52134e
Cyri0 <nemestamas94@gmail.com>
2023-11-21 18:44:52 +0100

    base MySQL_ERN app

---
+++ backend/index.ts
@@ -0,0 +1,19 @@
+import express, { Express, Request, Response } from 'express'
+import dotenv from 'dotenv'
+import path from 'path'
+
+dotenv.config()
+
+const app: Express = express()
+const port = process.env.PORT
+
+
+app.use(express.static(path.join(__dirname, '../../frontend/dist')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../frontend/dist', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`)
+})
